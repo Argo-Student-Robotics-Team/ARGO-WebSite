3a5a64ae10fcfd2bb3e1e5da1afb4f04b46dfe0c
Petar Nikolic <petnik033@gmail.com>
2024-09-22 20:49:58 +0200

    Your commit message

--- contact.js
+++ contact.js
@@ -2,11 +2,17 @@ function popup(event) {
     event.preventDefault();
     var link = event.target;
     var div = link.closest('.prikazi').nextElementSibling;
-    if (div.style.display === 'block') {
-        div.style.display = 'none';
+    if (div.classList.contains('show')) {
+        div.classList.remove('show');
+        setTimeout(function() {
+            div.style.display = 'none';
+        }, 500);
         link.innerHTML = '+';
     } else {
         div.style.display = 'block';
+        setTimeout(function() {
+            div.classList.add('show');
+        }, 10); 
         link.innerHTML = '-';
     }
 }
